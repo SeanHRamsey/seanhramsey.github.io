49ac2d44ffef80bed2671f566f35608bed5bc740
Sean Ramsey <47271111+SeanHRamsey@users.noreply.github.com>
2024-11-12 12:48:47 +1000

    message

--- sean-website/vite.config.js
+++ sean-website/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set to the repository name
+  base: '/seanhramsey.github.io/', // Set to the repository name
 });
