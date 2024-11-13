bbabad45b48e67e3f46c025b93c276fd694b6db4
Sean Ramsey <47271111+SeanHRamsey@users.noreply.github.com>
2024-11-13 19:40:41 +1000

    tests

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/seanhramsey.github.io/', // Set to the repository name
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
 });
+
